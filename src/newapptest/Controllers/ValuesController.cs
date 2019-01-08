4c6be64c7940ef0a03be8a97d6eebfe67b34b68e
ameeverm <ameeverm@ITEM-S43935>
2019-01-08 17:01:24 +0530

    new change

--- src/newapptest/Controllers/ValuesController.cs
+++ src/newapptest/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace newapptest.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "ameena" };
+            return new string[] { "value1", "ameena1" };
         }
 
         // GET api/values/5

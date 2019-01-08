e3f4263d295ca3f7b1818edc9ca784c51014f9fd
ameeverm <ameeverm@ITEM-S43935>
2019-01-08 11:45:04 +0530

    Add project files.

---
+++ src/newapptest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace newapptest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            // 2nd line added
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            // 3rd line added
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls(url) // 4th line added
+                .Build();
+
+            host.Run();
+        }
+    }
+}
